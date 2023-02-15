801d0ba5066e4e6495ed60b882b261a3de7f96ba
Thomas Jeffs <122478191+Agent00Muffin@users.noreply.github.com>
2023-02-14 21:47:45 -0500

    Finished Lab4
    Gateway Log investigation

--- lab4.py
+++ lab4.py
@@ -1,9 +1,13 @@
 from log_analysis import get_log_file_path_from_cmd_line, filter_log_by_regex
 import pandas as pd
+import re
 
 def main():
+    ip_address = "220.195.35.40"
     log_file = get_log_file_path_from_cmd_line(1)
     port_traffic = tally_port_traffic(log_file)
+    generate_invalid_user_report(log_file)
+    generate_source_ip_log(log_file, ip_address)
 
     for port_num, count in port_traffic.items():
         if count >= 100:
@@ -29,12 +33,25 @@ def generate_port_traffic_report(log_file, port_number):
     report_df.to_csv(f'destination_port_{port_number}_report.csv', index=False, header=header_row)
     return
 
-# TODO: Step 11
 def generate_invalid_user_report(log_file):
+
+    regex = r'(.{3} \d{2}) (\d{2}:\d{2}:\d{2}) myth sshd\[\d{5}]: Invalid user (\D+) from (\d{3}\.\d{3}\.\d{2}\.\d{2})'
+    data= filter_log_by_regex(log_file, regex)[1]
+
+    report_df = pd.DataFrame(data)
+    header_row =  ("Date", "Time", "Username", "IP Address")
+    report_df.to_csv(f'invalid_users.csv', index=False, header=header_row)
     return
 
 # TODO: Step 12
 def generate_source_ip_log(log_file, ip_address):
+
+    regex = r'(.+?) ' + f'SRC=({ip_address})' + r'(.+)'
+    data= filter_log_by_regex(log_file, regex)[1]
+
+    report_df = pd.DataFrame(data)
+    clean_ip = re.sub(r'\.', '_', f'{ip_address}')
+    report_df.to_csv(f'source_ip_{clean_ip}.log', index=False)
     return
 
 if __name__ == '__main__':
